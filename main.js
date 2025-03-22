bf8017da9652ebb7284b220828183cccbffa7ecc
TeRex777 <41856285+josexgonzalez@users.noreply.github.com>
2025-03-23 00:20:49 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1183,7 +1183,7 @@ async function main(userlandRW, wkOnly = false) {
     }
 
     // @ts-ignore
-    document.getElementById('top-bar-text').innerHTML = `Listening on: <span class="fw-bold">${ip.ip}</span> (port: ${ports}) (${ip.name})`;
+    document.getElementById('top-bar-text').innerHTML = `[/] Listening on: <span class="fw-bold">${ip.ip}</span> (port: ${ports}) (${ip.name})`;
 
     /** @type {Array<{payload_info: PayloadInfo, toast: HTMLElement}>} */
     let queue = [];
