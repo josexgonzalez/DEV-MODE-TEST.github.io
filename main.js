17e829f4c4df4ca074a8740aac717ce1a84132a6
TeRex777 <41856285+josexgonzalez@users.noreply.github.com>
2025-03-23 23:53:15 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -952,7 +952,7 @@ async function main(userlandRW, wkOnly = false) {
             is_etaHEN_running = true;
         } else {
             await log("etaHEN exited with non-zero code, port 9021 will likely not work", LogLevel.ERROR);
-            await new Promise(resolve => setTimeout(resolve, 6000));
+            await new Promise(resolve => setTimeout(resolve, 9000));
         }
 
 
