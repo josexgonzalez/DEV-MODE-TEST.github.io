86186329c70b4450151d85d4c5c7b8803b790c0c
TeRex777 <41856285+josexgonzalez@users.noreply.github.com>
2025-03-23 02:56:06 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -621,6 +621,27 @@ async function main(userlandRW, wkOnly = false) {
             return krw.ktextBase.add32(offset);
         }
 
+        await kread(data_base_addr.add32(OFFSET_KERNEL_DATA_BASE_SECURITYFLAGS));
+        let security_flags = p.read4(read_buf_store);
+        security_flags |= 0x14;  // securityflags |= 0x14
+        p.write4(read_buf_store, security_flags);
+
+        await copyin(data_base_addr.add32(OFFSET_KERNEL_DATA_BASE_SECURITYFLAGS), read_buf_store, 0x4);
+
+        await kread(data_base_addr.add32(OFFSET_KERNEL_DATA_BASE_PS4SDK));
+        let ps4sdk = p.read4(read_buf_store);
+        ps4sdk = 0x99999999;  // max kern.ps4_sdk_version
+        p.write4(read_buf_store, ps4sdk);
+
+        await copyin(data_base_addr.add32(OFFSET_KERNEL_DATA_BASE_PS4SDK), read_buf_store, 0x4);
+    
+        await kread(data_base_addr.add32(OFFSET_KERNEL_DATA_BASE_PS5SDK));
+        let ps5sdk = p.read4(read_buf_store);
+        ps5sdk = 0x99999999;  // max ps5sdk version
+        p.write4(read_buf_store, ps5sdk);
+
+        await copyin(data_base_addr.add32(OFFSET_KERNEL_DATA_BASE_PS5SDK), read_buf_store, 0x4);
+
         // Set security flags
         let security_flags = await krw.read4(get_kaddr(OFFSET_KERNEL_SECURITY_FLAGS));
         await krw.write4(get_kaddr(OFFSET_KERNEL_SECURITY_FLAGS), security_flags | 0x14);
