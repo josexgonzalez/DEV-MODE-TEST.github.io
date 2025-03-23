b44ef7a679333d8e8081bddbe9b08a618fd86fe9
TeRex777 <41856285+josexgonzalez@users.noreply.github.com>
2025-03-23 01:56:22 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -941,7 +941,7 @@ async function main(userlandRW, wkOnly = false) {
 
         if (await load_local_elf("etaHEN.bin") == 0) {
             await log(`elfldr listening on ${ip.ip}:9021`, LogLevel.INFO);
-            is_elfldr_running = true;
+            is_etaHEN_running = true;
         } else {
             await log("elfldr exited with non-zero code, port 9021 will likely not work", LogLevel.ERROR);
             await new Promise(resolve => setTimeout(resolve, 1000));
