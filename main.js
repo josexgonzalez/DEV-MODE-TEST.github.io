e5b4b4d79ee532180bd4621566d1458e97daff05
TeRex777 <41856285+josexgonzalez@users.noreply.github.com>
2025-03-23 23:24:47 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -584,9 +584,9 @@ async function main(userlandRW, wkOnly = false) {
 
     var load_payload_into_elf_store_from_local_file = async function (filename) {
         const response = await fetch('payloads/' + filename);
+        showTemporaryAlert("[+] Loading ELF file: " + filename + " ...", LogLevel.LOG);
         if (!response.ok) {
             throw new Error(`Failed to fetch the binary file. Status: ${response.status}`);
-            showTemporaryAlert("[+] Loading ELF file: " + filename + " ...", LogLevel.LOG);
         }
 
         const data = await response.arrayBuffer();
@@ -937,7 +937,7 @@ async function main(userlandRW, wkOnly = false) {
         }
 
         if (await load_local_elf("elfldr.bin") == 0) {
-            await log(`elfldr listening on ${ip.ip}:9020`, LogLevel.INFO);
+            await log(`elfldr listening on ${ip.ip}:9021`, LogLevel.INFO);
             is_elfldr_running = true;
         } else {
             await log("elfldr exited with non-zero code, port 9021 will likely not work", LogLevel.ERROR);
@@ -949,7 +949,7 @@ async function main(userlandRW, wkOnly = false) {
             is_etaHEN_running = true;
         } else {
             await log("etaHEN exited with non-zero code, port 9021 will likely not work", LogLevel.ERROR);
-            await new Promise(resolve => setTimeout(resolve, 4000));
+            await new Promise(resolve => setTimeout(resolve, 5000));
         }
 
         // const SOCK_NONBLOCK = 0x20000000; // for future reference, this is ignored if we're not jailbroken and explicitly setting it with fcntl returns SCE_KERNEL_ERROR_EACCES (at least on 4.03)
