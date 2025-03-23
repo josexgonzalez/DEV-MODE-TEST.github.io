3cc2d508c7464094f5942ec68662ea24380dae7c
TeRex777 <41856285+josexgonzalez@users.noreply.github.com>
2025-03-23 03:57:34 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -931,11 +931,19 @@ async function main(userlandRW, wkOnly = false) {
             }
         }
 
-        if (await load_local_elf("elfldr.elf") == 0) {
+        if (await load_local_elf("elfldr.bin") == 0) {
             await log(`elfldr listening on ${ip.ip}:9021`, LogLevel.INFO);
             is_elfldr_running = true;
         } else {
             await log("elfldr exited with non-zero code, port 9021 will likely not work", LogLevel.ERROR);
+            await new Promise(resolve => setTimeout(resolve, 1000));
+        }
+
+        if (await load_local_elf("etaHEN.bin") == 0) {
+            await log(`etaHEN listening on ${ip.ip}:9021`, LogLevel.INFO);
+            is_etaHEN_running = true;
+        } else {
+            await log("etaHEN exited with non-zero code, port 9021 will likely not work", LogLevel.ERROR);
             await new Promise(resolve => setTimeout(resolve, 1000));
         }
 
