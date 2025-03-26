69ff498306f16eb49f80125c081aa8249f13c219
TeRex777 <41856285+josexgonzalez@users.noreply.github.com>
2025-03-26 17:21:18 +0100

    Add files via upload

--- payloads/main.js
+++ payloads/main.js
@@ -936,12 +936,23 @@ async function main(userlandRW, wkOnly = false) {
             }
         }
 
-        if (await load_local_elf("etaHEN.bin") == 0) {
-            showTemporaryAlert(`etaHEN listening on ${ip.ip}:9021`, LogLevel.INFO);
+        if (await load_local_elf("etaHEN.elf") == 0) {
+            await log(`etaHEN listening on ${ip.ip}:9021`, LogLevel.INFO);
             is_etaHEN_running = true;
         } else {
-            showTemporaryAlert("etaHEN exited with non-zero code, port 9021 will likely not work", LogLevel.ERROR);
-            await new Promise(resolve => setTimeout(resolve, 1000));
+            await log("etaHEN exited with non-zero code, port 9021 will likely not work", LogLevel.ERROR);
+            await new Promise(resolve => setTimeout(resolve, 2000));
+        }
+
+        // Esperar 4 segundos antes de lanzar el siguiente payload
+        await new Promise(resolve => setTimeout(resolve, 9000));
+
+        if (await load_local_elf("elfldr.elf") == 0) {
+            await log(`elfldr listening on ${ip.ip}:9020`, LogLevel.INFO);
+            is_elfldr_running = true;
+        } else {
+            await log("elfldr exited with non-zero code, port 9021 will likely not work", LogLevel.ERROR);
+            await new Promise(resolve => setTimeout(resolve, 2000));
         }
 
 
