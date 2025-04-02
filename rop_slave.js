97415363c12636a048d9e6870314e6b7bafb563c
TeRex777 <41856285+josexgonzalez@users.noreply.github.com>
2025-04-02 21:28:14 +0200

    Add files via upload

--- rop_slave.js
+++ rop_slave.js
@@ -16,22 +16,26 @@ class KernelSyscalls {
     kekcall() {
         console.log("Ejecutando syscall: kekcall (0x100000027)");
         return this.syscall(0x100000027);
+        showTemporaryAlert("0x100000027");
     }
 
     kmem_alloc() {
         console.log("Ejecutando syscall: kmem_alloc (0x600000027)");
         let addr = this.syscall(0x600000027);
         return addr | 0xffffff8000000000n;
+        showTemporaryAlert("0x600000027");
     }
 
     kproc_create() {
         console.log("Ejecutando syscall: kproc_create (0x700000027)");
         return this.syscall(0x700000027);
+        showTemporaryAlert("0x700000027");
     }
 
     kstuff_check() {
         console.log("Ejecutando syscall: kstuff_check (0xffffffff00000027)");
         return this.syscall(0xffffffff00000027);
+        showTemporaryAlert("0xffffffff00000027");
     }
 }
 
