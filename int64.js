e2e3afe0def18a6ba25527a8502b5c935f9d9786
TeRex777 <41856285+josexgonzalez@users.noreply.github.com>
2025-04-02 21:05:49 +0200

    Add files via upload

--- int64.js
+++ int64.js
@@ -1,33 +1,7 @@
-// @ts-check
-
-function zeroFill(number, width) {
-    width -= number.toString().length;
-
-    if (width > 0) {
-        return new Array(width + (/\./.test(number) ? 2 : 1)).join('0') + number;
-    }
-
-    return number + ""; // always return a string
-}
-
-/**
- * Represents a 64-bit integer.
- * @constructor
- * @param {number} low - The lower 32 bits.
- * @param {number} hi - The higher 32 bits.
- * @returns {int64}
- */
 function int64(low, hi) {
-    // number >>> 0 = convert to unsigned
-    /** @type {number} */
     this.low = (low >>> 0);
-    /** @type {number} */
     this.hi = (hi >>> 0);
 
-    /** @type {Uint8Array|Uint16Array|Uint32Array} */
-    this.backing = null;
-
-    /** @this {int64} */
     this.add32inplace = function (val) {
         let new_lo = (((this.low >>> 0) + val) & 0xFFFFFFFF) >>> 0;
         let new_hi = (this.hi >>> 0);
@@ -38,14 +12,6 @@ function int64(low, hi) {
 
         this.hi = new_hi;
         this.low = new_lo;
-        if (this.backing !== null) {
-            if (this.backing.byteLength < val) {
-                throw new Error("int64.add32inplace: overflow");
-            }
-        
-            // this reuses the original backing buffer, so no big allocation here
-            this.backing = new Uint8Array(this.backing.buffer, val, this.backing.byteLength - val);
-        }
     }
 
     this.add32 = function (val) {
@@ -56,24 +22,13 @@ function int64(low, hi) {
             new_hi++;
         }
 
-        let ret = new int64(new_lo, new_hi);
-        if (this.backing !== null) {
-            if (this.backing.byteLength < val) {
-                throw new Error("int64.add32: overflow");
-            }
-        
-            // this reuses the original backing buffer, so no big allocation here
-            ret.backing = new Uint8Array(this.backing.buffer, val, this.backing.byteLength - val);
-        }
-
-        return ret;
+        return new int64(new_lo, new_hi);
     }
 
     this.sub32 = function (val) {
         let new_lo = (((this.low >>> 0) - val) & 0xFFFFFFFF) >>> 0;
         let new_hi = (this.hi >>> 0);
 
-        // @ts-ignore
         if (new_lo > (this.low) & 0xFFFFFFFF) {
             new_hi--;
         }
@@ -81,12 +36,10 @@ function int64(low, hi) {
         return new int64(new_lo, new_hi);
     }
 
-    /** @this {int64} */
     this.sub32inplace = function (val) {
         let new_lo = (((this.low >>> 0) - val) & 0xFFFFFFFF) >>> 0;
         let new_hi = (this.hi >>> 0);
 
-        // @ts-ignore
         if (new_lo > (this.low) & 0xFFFFFFFF) {
             new_hi--;
         }
@@ -107,24 +60,27 @@ function int64(low, hi) {
         return new int64(new_lo, new_hi);
     }
 
-    /** 
-     * @param {number} radix
-     * @returns {string}
-     */
-    this.toString = function (radix = 16) {
-        let lo_str = (this.low >>> 0).toString(radix);
-        let hi_str = (this.hi >>> 0).toString(radix);
+    this.toString = function () {
+        let lo_str = (this.low >>> 0).toString(16);
+        let hi_str = (this.hi >>> 0).toString(16);
 
-        if (this.hi == 0) {
+        if (this.hi == 0)
             return lo_str;
-        } else {
-            // explicit check for 16 so its faster since in practice this is all thats used
-            const width = radix === 16 ? 8 : Math.ceil(32 / Math.log2(radix));
-            lo_str = zeroFill(lo_str, width);
-        }
+        else
+            lo_str = zeroFill(lo_str, 8)
 
         return hi_str + lo_str;
     }
 
     return this;
+}
+
+function zeroFill(number, width) {
+    width -= number.toString().length;
+
+    if (width > 0) {
+        return new Array(width + (/\./.test(number) ? 2 : 1)).join('0') + number;
+    }
+
+    return number + ""; // always return a string
 }
