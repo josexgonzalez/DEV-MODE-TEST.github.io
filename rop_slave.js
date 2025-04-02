76eef532d076d0b1dc2fa6891733fd80e3eb16b1
TeRex777 <41856285+josexgonzalez@users.noreply.github.com>
2025-04-02 21:21:44 +0200

    Add files via upload

--- rop_slave.js
+++ rop_slave.js
@@ -4,4 +4,46 @@ self.onmessage = function (event) {
     event.ports[0].postMessage(1);
 }
 
+class LinuxLoader {
+    constructor(p) {
+        this.p = p; // Primitivas de lectura/escritura en kernel
+    }
+
+    syscall(num, arg1 = 0, arg2 = 0, arg3 = 0, arg4 = 0, arg5 = 0, arg6 = 0) {
+        return this.p.syscall(num, arg1, arg2, arg3, arg4, arg5, arg6);
+    }
+
+    allocateKernelMemory(size) {
+        console.log(`Asignando ${size} bytes en kernel...`);
+        let addr = this.syscall(0x600000027, size);
+        return addr | 0xffffff8000000000n;
+    }
+
+    writePayload(addr, payload) {
+        console.log(`Escribiendo payload en ${addr.toString(16)}...`);
+        for (let i = 0; i < payload.length; i += 8) {
+            let chunk = payload.slice(i, i + 8).reduce((acc, byte, index) => acc | (BigInt(byte) << (8n * BigInt(index))), 0n);
+            this.p.write8(addr + BigInt(i), chunk);
+        }
+    }
+
+    executeKernelPayload() {
+        console.log("Ejecutando payload...");
+        return this.syscall(0x700000027);
+    }
+
+    loadLinux(kernelPayload) {
+        let addr = this.allocateKernelMemory(kernelPayload.length);
+        this.writePayload(addr, kernelPayload);
+        return this.executeKernelPayload();
+    }
+}
+
+// Supongamos que ya tienes acceso a `p` desde el exploit
+const linuxLoader = new LinuxLoader(p);
+
+// Payload del cargador de Linux (debe ser un array de bytes)
+const linuxPayload = [...]; // Cargar binario del payload aquí
+
+linuxLoader.loadLinux(linuxPayload);
 
