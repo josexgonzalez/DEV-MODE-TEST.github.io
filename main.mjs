5ba27cd26c0dac78c62812f41078d8b0f2aa59ed
TeRex777 <41856285+josexgonzalez@users.noreply.github.com>
2025-05-08 17:56:08 +0200

    Add files via upload

---
+++ main.mjs
@@ -0,0 +1,15 @@
+// main.mjs - Punto de entrada para PSFree
+import './alert.mjs';
+import './config.mjs';
+import './send.mjs';
+
+import './module/int64.mjs';
+import './module/mem.mjs';
+import './module/memtools.mjs';
+import './module/offset.mjs';
+import './module/rw.mjs';
+import './module/utils.mjs';
+import './module/view.mjs';
+import './module/chain.mjs';
+
+import './psfree.mjs'; // Debe cargarse al final
